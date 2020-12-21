6cfec3a09b5cdfa73dddb7a92e7f4b10023cbf03
3541 <3541ax@gmail.com>
2020-12-21 15:29:27 -0800

    Do not enable sanitizers by default on clang+Windows.

--- cmake/Sanitize.cmake
+++ cmake/Sanitize.cmake
@@ -1,6 +1,11 @@
 option(SANITIZE "Enable sanitizers.")
 function(target_add_sanitizers target default_on)
   include(AddFlags)
+
+  if(WIN32 AND CMAKE_C_COMPILER_ID STREQUAL Clang)
+    set(default_on FALSE)
+  endif()
+
   if (SANITIZE OR ${default_on})
     target_add_flags(${target} "fsanitize=address;fsanitize=undefined" TRUE FALSE)
   endif()
