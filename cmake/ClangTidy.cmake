c8b5199876af02b51a90f400cd0524cf80a5e914
Alex O'Brien <3541ax@gmail.com>
2021-02-12 12:44:43 +1100

    CMAKE: Fix typo.

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -10,7 +10,7 @@ function(target_add_clang_tidy target)
   # The "secure" C11 API which this lint requires is neither particularly more
   # secure than the alternative, nor supported on any major C library other
   # than Microsoft's (and even theirs is out of date).
-  set(CLANG_TIDY_CMD "${CLANG_TIDY};-checks=-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling;-extra-arg=-Wno-unknown-warning-option,-extra-arg=-Qunused-arguments")
+  set(CLANG_TIDY_CMD "${CLANG_TIDY};-checks=-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling;-extra-arg=-Wno-unknown-warning-option;-extra-arg=-Qunused-arguments")
 
   set_target_properties(${target} PROPERTIES
     C_CLANG_TIDY "${CLANG_TIDY_CMD}"
