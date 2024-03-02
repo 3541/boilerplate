c923c28c44d07c79a81a4f8552f8de085925c427
Alex O'Brien <3541@3541.website>
2024-03-03 10:39:37 +1100

    FIX: Work around GCC bug 98735 by disabling UBSAN
    When compiling C++ modules with UBSAN enabled, GCC will ICE. This patch
    works around the issue by disabling UBSAN if using GCC with C++ modules
    (indicated by a new optional flag).

--- cmake/Sanitize.cmake
+++ cmake/Sanitize.cmake
@@ -3,11 +3,17 @@ option(SANITIZE_FORCE_OFF "Disable sanitizers." FALSE)
 function(target_add_sanitizers target default_on)
   include(AddFlags)
 
-  if(WIN32 AND CMAKE_C_COMPILER_ID STREQUAL Clang)
+  if(WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL Clang)
     set(default_on FALSE)
   endif()
 
   if (SANITIZE OR ${default_on} AND NOT SANITIZE_FORCE_OFF)
-    target_add_flags(${target} "fsanitize=address;fsanitize=undefined" TRUE FALSE)
+    target_add_flags(${target} "fsanitize=address" TRUE FALSE)
+
+    list(FIND ARGN "MODULES" modules)
+    if (NOT CMAKE_CXX_COMPILER_ID STREQUAL GNU AND modules GREATER -1)
+      # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=98735
+      target_add_flags(${target} "fsanitize=undefined" TRUE FALSE)
+    endif()
   endif()
 endfunction()
